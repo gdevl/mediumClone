c116989eba5fab5cc5689d6b37883e159c58be44
mark <markmcclatchy@gmail.com>
2020-10-03 18:26:50 -0400

    init setup for front and back ends

---
+++ backEndServer/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+app.use('/', indexRouter);
+
+/************************************************/
+
+// Catch unhandled requests and forward to error handler.
+app.use((req, res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.status = 404;
+    next(err);
+});
+
+
+//TODO Custom error handlers.
+
+
+// Generic error handler.
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    const isProduction = environment === "production";
+    res.json({
+        title: err.title || "Server Error",
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
+
+
+/************************************************/
+module.exports = app;
---
+++ backEndServer/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const { db } = require('../db/models');
+
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
