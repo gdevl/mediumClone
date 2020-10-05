8da8a97bd4c1be5f6249d5dc7c8df1d0de9d6c28
gdevl <throwawayemailaddy@protonmail.com>
2020-10-05 17:35:07 -0400

    many-to-many association between Users and Stories added

--- db/models/user.js
+++ db/models/user.js
@@ -33,7 +33,7 @@ module.exports = (sequelize, DataTypes) => {
     {}
   );
   User.associate = function (models) {
-    // associations can be defined here
+    User.hasMany(models.Story, { foreignKey: "id" });
   };
   return User;
 };
