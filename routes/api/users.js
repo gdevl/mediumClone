4bf5e4fbca791520e0f65b77ae53f5ea2a275017
David <dyclee@umich.edu>
2020-10-05 16:02:26 -0500

    working on signup post route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const { handleValidationErrors, signUpValidator } = require('../../validations');
 const {asyncHandler} = require('../../utils');
+const { route } = require('../../app');
 
 
 const router = express.Router();
+
+router.post('/', signUpValidator, handleValidationErrors, asyncHandler(async(req,res,next) => {
+    const { username, password, email, firstName, lastName, bio, avatarUrl } = req.body;
+
+}))
+
+
+module.exports = router;
