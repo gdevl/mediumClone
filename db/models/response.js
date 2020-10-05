8a7f49047c46b771fdba01dcaff0ceef77af368b
gdevl <throwawayemailaddy@protonmail.com>
2020-10-05 19:08:12 -0400

    response model and migration added. belongsTo associations defined on response side only

---
+++ db/models/response.js
@@ -0,0 +1,26 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Response = sequelize.define(
+    "Response",
+    {
+      content: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      storyId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {}
+  );
+  Response.associate = function (models) {
+    Response.belongsTo(models.User, { foreignKey: "userId" });
+    Response.belongsTo(models.Story, { foreignKey: "storyId" });
+  };
+  return Response;
+};
