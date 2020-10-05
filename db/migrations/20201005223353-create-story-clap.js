c1b7222b9e902fdb14bdd2692aa1c3a0bd0b1e17
gdevl <throwawayemailaddy@protonmail.com>
2020-10-05 18:46:08 -0400

    storyclap model/migration added. associations established.

---
+++ db/migrations/20201005223353-create-story-clap.js
@@ -0,0 +1,34 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("StoryClaps", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      storyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Stories" },
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Users" },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("StoryClaps");
+  },
+};
---
+++ db/models/storyclap.js
@@ -0,0 +1,22 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const StoryClap = sequelize.define(
+    "StoryClap",
+    {
+      storyId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {}
+  );
+  StoryClap.associate = function (models) {
+    StoryClap.belongsTo(models.Story, { foreignKey: "storyId" });
+    StoryClap.belongsTo(models.User, { foreignKey: "userId" });
+  };
+  return StoryClap;
+};
