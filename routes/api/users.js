a702c1b5c30e4b950a09d6ff58274fded299b98c
David <dyclee@umich.edu>
2020-10-05 16:28:52 -0500

    created jwt token function, using it users post route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const { User } = require('../../db/models');
+const {getUserToken} = require('../../config/auth');
+
 const { handleValidationErrors, signUpValidator } = require('../../validations');
 const {asyncHandler} = require('../../utils');
 const { route } = require('../../app');
@@ -6,9 +10,28 @@ const { route } = require('../../app');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    res.render('sign-up');
+})
+
 router.post('/', signUpValidator, handleValidationErrors, asyncHandler(async(req,res,next) => {
     const { username, password, email, firstName, lastName, bio, avatarUrl } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 16);
+    const user = await User.create({
+        username,
+        email,
+        firstName,
+        lastName,
+        bio,
+        avatarUrl,
+        hashedPassword,
+    });
 
+    const token = getUserToken(user);
+    res.status(201).json({
+        user: {id:user.id},
+        token,
+    })
 }))
 
 
