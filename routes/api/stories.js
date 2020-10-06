9f77436c03b1de795b4ba98edecfb4dd9617e5c0
David <dyclee@umich.edu>
2020-10-06 15:36:38 -0500

    completed clap creation route

--- routes/api/stories.js
+++ routes/api/stories.js
@@ -22,3 +22,48 @@ router.post('/', storyValidator, handleValidationErrors, asyncHandler(async (req
 }))
 
 module.exports = router;
+const { StoryClap, Story, User } = require('../../db/models');
+// const { getUserToken } = require('../../config/auth');
+
+const { asyncHandler } = require('../../utils');
+const {
+    handleValidationErrors,
+    signUpValidator,
+    loginValidator
+} = require('../../validations');
+
+
+const router = express.Router();
+
+// story not found error handler
+
+function clapAlreadyExistsError(id) {
+    let error = new Error(`Story with ${id} has already been clapped.`);
+    error.title = "Clap already exists.";
+    error.status = 400;
+    return error;
+  }
+
+//not sure about this story clap route
+router.post('/:id(\\d+)', asyncHandler (async (req, res, next) => {
+    const storyId = req.params.id;
+    const userClap = await StoryClap.findAll({
+        where: {
+            userId: req.user.id,
+            storyId: storyId
+        }
+    })
+
+    if (userClap.length === 0) {
+        const newStoryClap = await StoryClap.create({
+            userId: req.user.id,
+            storyId: storyId,
+        });
+        res.json(newStoryClap);
+    } else {
+        next(clapAlreadyExistsError(storyId))
+    }
+}));
+
+
+module.exports = router;
