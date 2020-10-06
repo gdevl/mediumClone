717c986ddf4dec852467f4cb9a1f9d160a9e9bb2
Brian Wang <brbwang@yahoo.com>
2020-10-06 18:36:16 -0400

    fixed url in get

--- routes/api/users.js
+++ routes/api/users.js
@@ -70,7 +70,7 @@ router.post(
 );
 
 router.get(
-  "/id(\\d+)",
+  "/users/id(\\d+)",
   asyncHandler(async (req, res, next) => {
     const id = req.params.id;
     const stories = await Stories.findAll({ where: {userId:id}});
