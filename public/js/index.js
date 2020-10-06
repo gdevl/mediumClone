295174be9eaded9a38fb5e1c47399d43f8ed0b74
mark <markmcclatchy@gmail.com>
2020-10-06 03:15:39 -0400

    log-in and sign-up box dynamically opens and closes

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     document
         .querySelector('.nav-bar__sign-up-btn')  //TODO Get real class name
-        .addEventListener('click', () => {
-            console.log('Sign Up Clicked');
-            showSignUpBox()
-        })
+        .addEventListener('click', showSignUpBox)
     
     document
-        .querySelector('.close-overlay')
+        .querySelector('.close-sign-up')
         .addEventListener('click', hideSignUpBox)
     
-    console.log('DOM Loaded...')
+    document
+        .querySelector('.nav-bar__log-in-btn')
+        .addEventListener('click', showLogInBox)
+        
+    document
+        .querySelector('.close-log-in')
+        .addEventListener('click', hideLogInBox)
     
 })
 
@@ -24,5 +27,18 @@ const showSignUpBox = () => {
 const hideSignUpBox = () => {
     document
         .querySelector('.sign-up-overlay')
+        .classList.add('hidden')
+}
+
+const showLogInBox = () => {
+    document
+        .querySelector('.log-in-overlay')
+        .classList.remove('hidden')
+}
+
+const hideLogInBox = () => {
+    console.log('hide log in')
+    document
+        .querySelector('.log-in-overlay')
         .classList.add('hidden')
 }
