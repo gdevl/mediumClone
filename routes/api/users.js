410f1605fcd610f5b5497b60271c8198173b09a9
Brian Wang <brbwang@yahoo.com>
2020-10-06 18:41:52 -0400

    fixed regex

--- routes/api/users.js
+++ routes/api/users.js
@@ -72,6 +72,7 @@ router.get(
   "/users/:id(\\d+)",
   asyncHandler(async (req, res, next) => {
     const id = req.params.id;
+    console.log(id);
     const stories = await Stories.findAll({ where: {userId:id}});
     res.render('user', { stories })
   })
