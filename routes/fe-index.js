5d2f5e0f68916b4a71403f3d70bdfd07a114b571
mark <markmcclatchy@gmail.com>
2020-10-08 13:40:31 -0400

    trending pug, css, and fe-index updated

--- routes/fe-index.js
+++ routes/fe-index.js
@@ -1,12 +1,28 @@
 const express = require("express");
-const { User, Story, Follow } = require("../db/models");
-const { getUserToken } = require("../config/auth");
 
-const { asyncHandler } = require("../utils");
+const { User, Story, StoryClap, Follow } = require("../db/models");
+const { asyncHandler, createTrendingStories } = require("../utils");
+
 
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res, next) => {
+  const topStoryClaps = await StoryClap.findAll({
+    // where: {
+    //   createdAt: {
+    //       [Op.gte]: [moment().subtract(7, 'days').toDate()]
+    //   }
+    // },
+    include: { 
+      model: Story, 
+      include: { model: User }
+    },
+    limit: 6,
+    order: [['storyId', 'DESC']],
+  });
+  
+  const trendingStoriesData = createTrendingStories(topStoryClaps);
+  
   const stories = await Story.findAll({
     limit: 5,
     order: [['userId', 'DESC']],
@@ -14,8 +30,8 @@ router.get('/', asyncHandler(async (req, res, next) => {
       model: User,
     }
   })
-  // console.log(sstories)
-  res.render('home', {stories});
+
+  res.render('home', { stories, trendingStoriesData });
 }))
 
 module.exports = router;
