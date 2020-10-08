28e98a495f86c308ab86eca75dfa3825141c2e39
Brian Wang <brbwang@yahoo.com>
2020-10-08 15:44:40 -0400

    follow button now adds follow to database

--- public/js/follow.js
+++ public/js/follow.js
@@ -1,15 +1,16 @@
 import { handleErrors } from "./utils.js";
 
 document.addEventListener("DOMContentLoaded", ()=>{
-  document.getElementById('follow').addEventListener('click', postFollow);
+  const button = document.getElementById('follow')
+  button.addEventListener('click', postFollow(button.value));
 })
 
-const postFollow = async () => {
-  const followedId = this.value;
+const postFollow = async (val) => {
+  const followedId = val;
   const followerId = localStorage.getItem('MEDIUM_CLONE_CURRENT_USER_ID')
   const body = { followerId, followedId }
   try {
-    const res = await fetch('/api/stories', {
+    const res = await fetch('/api/follow', {
       method: 'POST',
       body: JSON.stringify(body),
       headers: {
--- routes/api/follow.js
+++ routes/api/follow.js
@@ -5,13 +5,11 @@ const { asyncHandler } = require('../../utils');
 
 const router = express.Router();
 
-router.post('/:id(\\d+)', asyncHandler(async (req, res, next) => {
-  const followerId = localStorage.getItem('MEDIUM_CLONE_CURRENT_USER_ID');
-  const followedId = req.params.id;
-  
-  const followed = await Follow.create({followerId, followedId})
+router.post('/', asyncHandler(async (req, res, next) => {
+  const { followerId, followedId } = req.body;
+  const newFollow = await Follow.create({followerId, followedId})
 
-  res.status(201).json({followed});
+  res.status(201).json( { newFollow } );
 }))
 
 module.exports = router;
