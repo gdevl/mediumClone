062d262a13c2cbc7b86a63bb15bbfc19bb307241
gdevl <throwawayemailaddy@protonmail.com>
2020-10-09 19:09:34 -0400

    tinkered with config/database

--- config/database.js
+++ config/database.js
@@ -15,8 +15,7 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    use_env_variable:
-      "postgres://dvdqgvhemqvvnj:6c3c451a5b508e9ded4dcc38e86924aa9b17cc43312a561cfb52d0e9b359e4ed@ec2-3-210-178-167.compute-1.amazonaws.com:5432/ddi8sj63mpq6mu",
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     seederStorage: "sequelize",
   },
