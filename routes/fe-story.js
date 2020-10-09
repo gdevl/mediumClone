0d4f736bd130b11d7aa22257b8463954fb5262ec
mark <markmcclatchy@gmail.com>
2020-10-09 12:56:14 -0400

    story body section complete

--- routes/fe-story.js
+++ routes/fe-story.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const { asyncHandler, formatDate, determineReadTime } = require("../utils");
-const { User, Story } = require('../db/models');
+const { User, Story, StoryClap, Response } = require('../db/models');
 
 
 router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
@@ -11,13 +11,22 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
     const storyData = await Story.findOne({ 
         where: { id: storyId },
         include: User,
-        attributes: [
-            'id', 
-            'title', 
-            'subtitle', 
-            'content', 
-            'updatedAt',
-        ]
+        // attributes: [
+        //     'id', 
+        //     'title', 
+        //     'subtitle', 
+        //     'content', 
+        //     'updatedAt',
+        //     'imageUrl'
+        // ]
+    });
+    
+    const storyClaps = await StoryClap.findAndCountAll({
+        where: { storyId: storyId }
+    });
+    
+    const storyResponses = await Response.findAndCountAll({
+        where: { storyId: storyId }
     });
     
     const story = {
@@ -25,54 +34,19 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
         title: storyData.title,
         subtitle: storyData.subtitle,
         content: storyData.content,
+        imageUrl: storyData.imageUrl,
         readTime: determineReadTime(storyData.content),
         date: formatDate(storyData.updatedAt),
         authorId: storyData.User.id,
         authorName: `${storyData.User.firstName} ${storyData.User.lastName}`,
         avatarUrl: storyData.User.avatarUrl,
-        bio: storyData.User.bio
+        bio: storyData.User.bio,
+        clapsCount: storyClaps.count,
+        responsesCount: storyResponses.count
     }
     res.render('story-page', { story });
-    // res.json(story)
 }));
 
-
-// router.get("/:id(\\d+)", asyncHandler(async (req, res, next) => {
-//     const storyId = parseInt(req.params.id, 10);
-//     const storyJSON = await fetch(`/stories/${storyId}`);
-//     const story = await storyJSON.json();
-//     res.render('story-page', { story });
-// }));
-
-
-
-//*Attempt to find Responses Count
-// const storyId = parseInt(req.params.id, 10);
-//     const storyData = await Story.findOne({ 
-//         group: ['Story.id'],
-//         where: { id: storyId },
-//         include: [{ model: User }, { model: Response, attributes: []}],
-//         attributes: [
-//             'id', 
-//             'title', 
-//             'subtitle', 
-//             'content', 
-//             'updatedAt',
-//             [sequelize.fn('COUNT', sequelize.col('Response.id')), 'num_responses'],
-//         ]
-//     });
-//     console.log('STORY DATA: ', storyData)
-//     const story = {
-//         id: storyData.id,
-//         title: storyData.title,
-//         subtitle: storyData.subtitle,
-//         content: storyData.content,
-//         date: formatDate(storyData.updatedAt),
-//         authorName: `${storyData.User.firstName} ${storyData.User.lastName}`,
-//         avatarUrl: storyData.User.avatarUrl,
-//         bio: storyData.User.bio
-//     }
-//     res.render('story-page', { story });
 
 
 
