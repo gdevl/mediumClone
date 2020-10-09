5f2895be2abe0b3b456d81807e295e1c0a2dd1f9
mark <markmcclatchy@gmail.com>
2020-10-09 16:10:30 -0400

    left side panel complete

--- public/js/story.js
+++ public/js/story.js
@@ -1,6 +1,16 @@
-window.scroll(() => {
-    const leftSidePanel = document.querySelector('.left-side-panel')
-    if (window.scrollTop() > 100) {
-        
+
+document.addEventListener('scroll', () => {
+    const leftSidePanel = document.querySelector('.main__left-side-panel');
+    
+    if (window.pageYOffset > 300) {
+        leftSidePanel.style.opacity = 1;
     }
-})
+    else {
+        leftSidePanel.style.opacity = 0;
+    }
+});
+
+const url = window.location.href;
+const storyId = url.match('\\d+$')[0];
+
+
