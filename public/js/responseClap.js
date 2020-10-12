8c7d926214b3749afb7dff17d90cd9a3f357d426
David <dyclee@umich.edu>
2020-10-11 20:20:18 -0500

    working on fetch calls

--- public/js/responseClap.js
+++ public/js/responseClap.js
@@ -8,7 +8,35 @@ document.addEventListener("DOMContentLoaded", (e) => {
 });
 
 const responseClapping = (element) => {
-    console.log("ELEMENT: ", element);
-    console.log(element.dataset.value);
-    console.log(element.dataset.responseId);
+    // console.log("ELEMENT: ", element);
+    // console.log(element.dataset.value);
+    // console.log(element.dataset.responseId);
+    if (element.dataset.value === "toBeClapped") {
+        element.dataset.value = "unclap";
+        element.src = "/images/clapped3.png";
+        postResponseClap(element.dataset.responseId);
+    } else if (element.dataset.value === "unclap") {
+        element.dataset.value = "toBeClapped";
+        element.src = "/images/clapping1.png";
+        removeResponseClap(element.dataset.responseId);
+    } else {
+        alert('Currently not signed in.');
+    }
+}
+
+const postResponseClap = async (responseId) => {
+    const userId = localStorage.getItem("MEDIUM_CLONE_CURRENT_USER_ID");
+    const body = { userId, responseId };
+
+    const res = await fetch(`/api/responses/${responseId}/clap`, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (res.status === 401) {
+        return;
+    };
+
 }
